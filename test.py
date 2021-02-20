cc227962ef4e4a1542b0fff3bba25c4935d81d7c
Yash Nag <yash@eder.io>
2021-02-21 01:13:15 +0530

    printing response

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ import requests
 
 def test_1():
     response = requests.get("http://localhost:80/items/5?q=somequery")
-    assert response['item_id'] == 5 
-    assert response['item_id'] == "somequery"
+    print (response)
+    # assert response['item_id'] == 5 
+    # assert response['item_id'] == "somequery"
 
 test_1()
